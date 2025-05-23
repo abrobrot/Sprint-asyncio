bd061cc9f38ed179a9b911fedf5038b5ddb152cc
abrobrot <olegstakhov210@gmail.com>
2025-05-23 20:09:42 +0300

    Add files via upload

---
+++ task8!.py
@@ -0,0 +1,39 @@
+import asyncio
+
+
+class RepeatTask:
+
+
+    def __init__(self):
+        self.counter = 0
+        self.task = None
+
+
+    async def task_r(self):
+        self.counter += 1
+        print ("Программа выполнилась ", self.counter, "раз" )
+
+
+    async def repeat(self, max_runs: int = 5):
+        while True:
+            if self.counter >= max_runs:
+                if self.task:
+                    self.task.cancel()
+                break
+            await self.task_r()
+            await asyncio.sleep(1)
+
+
+def cb(task):
+    print("Выполнение закончено!")
+
+
+async def main():
+    repeat_task = RepeatTask()
+    task = asyncio.create_task(repeat_task.repeat())
+    task.add_done_callback(cb)
+    await task
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
